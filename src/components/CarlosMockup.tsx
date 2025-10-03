e042387b4f2d8c5ac192f262a6dbff0a3f9cbaf4
Alberto <alberto.alvarez@applieditweb.com>
2025-10-03 11:32:19 +0200

    Implement code changes to enhance functionality and improve performance

--- src/components/CarlosMockup.tsx
+++ src/components/CarlosMockup.tsx
@@ -1,3 +1,7 @@
+import points from "../data/processed_points.json";
 const CarlosMockup = () => {};
-
+//1 Crear mapa de maplibre con los points importados
+//2 Aplicar clustering para mejorar el rendimiento y la visualizacion
+//3 Aplicar filtros para la visualizacion de puntos: filter by tag, filter by gnss > 0 , filter by mac
+//4 Animar con una line el recorrido secuencial entre los puntos dado la propiedad id de cada punto.
 export default CarlosMockup;
